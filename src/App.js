2a21a1f558a3fe0606d5a14fcf2105a018831aed
ColiJD <colindresj9@gmail.com>
2023-05-18 11:50:00 -0600

    Utilizando libreria UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import ClassComponets from './componentes/ClassComponents';
-
-
+import FormSignUp from "./componentes/FormSigntUp";
+import { Container } from '@mui/material';
+import Typography from '@mui/material/Typography';
 function App() {
   return (
-    <div >
-      <ClassComponets/>
-    </div>
+    <Container component="section" maxWidth="sm">
+      <Typography variant="h3" align="center" component="h1">Formulario Registro</Typography>
+      <FormSignUp />
+    </Container>
   );
 }
 
--- src/componentes/FormSigntUp.jsx
+++ src/componentes/FormSigntUp.jsx
@@ -1,6 +1,46 @@
-function FormSignUp (){
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+
+function FormSignUp() {
     return <form>
-        <input type="text" />
+        <TextField
+            id='name'
+            label='Nombre'
+            variant='outlined'
+            margin='normal'
+            fullWidth
+        />
+        <TextField
+            id='lastname'
+            label='Apellido'
+            variant='outlined'
+            margin='normal'
+            fullWidth
+        />
+        <TextField
+            id='email'
+            label='Correo'
+            variant='outlined'
+            type='email'
+            margin='normal'
+            fullWidth
+        />
+        <FormGroup>
+            <FormControlLabel
+                control={<Switch
+                    defaultChecked={true}
+                />} label="Promociones" />
+            <FormControlLabel
+                control={<Switch
+                    defaultChecked={true}
+                />} label="Novedades" />
+        </FormGroup>
+        <Button variant='contained' type="submit">Registrarse</Button>
+
 
     </form>
 }
